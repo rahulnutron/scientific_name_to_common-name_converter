ca57715390f5c6b02a430053d16dfb54c206ced4
Rahul Shaw <rahul.nutron@gmail.com>
2021-10-19 13:55:44 +0200

    Update convert_scientific_name_to_common_names.py

--- convert_scientific_name_to_common_names.py
+++ convert_scientific_name_to_common_names.py
@@ -38,3 +38,32 @@ def convert(path = 'C:/Users/rahul/',specfile="speclist.txt",outfile="Sci2Com.cs
     return dic, dic_rev
 
 #convert()
+
+def concise_fasta_header(spaceOrUnderscore = ' ',path='C:/Users/rahul/Teaching/',d_file = 'Human_HBB-Alignment-Descriptions.csv',f_file='seqdump_human_HBB_homolog.fas',outfile='human_HBB_homolog.fas'):
+    dic = {}
+    des_file = open(path+d_file,'r')
+    fas_file = open(path+f_file,'r')
+    fw = open(path+outfile,'w')
+    for line in des_file:
+        line = line.replace('"','')
+        s = line.split(',')
+        if len(s)>1:
+            dic[s[1]] = s[2]
+    des_file.close()
+    for line in fas_file:
+        if '>' in line:
+            line = line.strip('\n')
+            s = line.split('[')
+            N = s[-1:][0].strip('[]')
+            pid = s[0].split()[0].strip('>[]')
+            if N in dic.keys() and dic[N]!='NA':
+                fw.write('>'+pid+spaceOrUnderscore+dic[N]+'\n')
+            elif N in dic.keys() and dic[N]=='NA':
+                fw.write('>'+pid+spaceOrUnderscore+N+'(SN)\n')
+            else:
+                fw.write(line+'\n')
+        else:
+            fw.write(line)
+    fas_file.close()
+    fw.close()
+    return dic
