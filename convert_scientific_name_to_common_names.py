839ec9fa7063c1bd879f3d830c751fe9145829b6
Rahul Shaw <rahul.nutron@gmail.com>
2021-10-19 15:53:34 +0200

    Update convert_scientific_name_to_common_names.py

--- convert_scientific_name_to_common_names.py
+++ convert_scientific_name_to_common_names.py
@@ -39,7 +39,7 @@ def convert(path = 'C:/Users/rahul/',specfile="speclist.txt",outfile="Sci2Com.cs
 
 #convert()
 
-def concise_fasta_header(spaceOrUnderscore = ' ',path='C:/Users/rahul/Teaching/',d_file = 'Human_HBB-Alignment-Descriptions.csv',f_file='seqdump_human_HBB_homolog.fas',outfile='human_HBB_homolog.fas'):
+def concise_fasta_header(repeat_entry = True,keep_des=True,spaceOrUnderscore = ' ',path='C:/Users/rahul/',d_file = 'Human_HBB-Alignment-Descriptions.csv',f_file='seqdump_human_HBB_homolog.fas',outfile='human_HBB_homolog.fas'):
     dic = {}
     des_file = open(path+d_file,'r')
     fas_file = open(path+f_file,'r')
@@ -50,20 +50,43 @@ def concise_fasta_header(spaceOrUnderscore = ' ',path='C:/Users/rahul/Teaching/'
         if len(s)>1:
             dic[s[1]] = s[2]
     des_file.close()
+    sn_list = []
     for line in fas_file:
-        if '>' in line:
-            line = line.strip('\n')
-            s = line.split('[')
-            N = s[-1:][0].strip('[]')
-            pid = s[0].split()[0].strip('>[]')
-            if N in dic.keys() and dic[N]!='NA':
-                fw.write('>'+pid+spaceOrUnderscore+dic[N]+'\n')
-            elif N in dic.keys() and dic[N]=='NA':
-                fw.write('>'+pid+spaceOrUnderscore+N+'(SN)\n')
+        if repeat_entry:
+            if '>' in line:
+                line = line.strip('\n')
+                s = line.split('[')
+                N = s[-1:][0].strip('[]')
+                if keep_des:
+                    pid = s[0].strip('[]')
+                else:
+                    pid = s[0].split()[0].strip('[]')
+                if N in dic.keys() and dic[N]!='NA':
+                    fw.write(pid+spaceOrUnderscore+'('+dic[N]+')'+'\n')
+                elif N in dic.keys() and dic[N]=='NA':
+                    fw.write(pid+spaceOrUnderscore+'('+N+'(SN))\n')
+                else:
+                    fw.write(line+'\n')
             else:
-                fw.write(line+'\n')
-        else:
-            fw.write(line)
+                fw.write(line)
+        else:                          # false not working
+            pass
+##            line = line.strip('\n')
+##            s = line.split('[')
+##            N = s[-1:][0].strip('[]')
+##            if '>' in line:
+##                if N not in sn_list:
+##                    pid = s[0].split()[0].strip('>[]')
+##                    if N in dic.keys() and dic[N]!='NA':
+##                        fw.write('>'+pid+spaceOrUnderscore+dic[N]+'\n')
+##                    elif N in dic.keys() and dic[N]=='NA':
+##                        fw.write('>'+pid+spaceOrUnderscore+N+'(SN)\n')
+##                    else:
+##                        fw.write(line+'\n')
+##            else:
+##                if N not in sn_list:
+##                    fw.write(line+'\n')
+        sn_list.append(N)
     fas_file.close()
     fw.close()
     return dic
